595507d3a3c5e769483e7583851a1277da1839c8
subham mohanty <smohanty2628@gmail.com>
2026-07-18 18:48:06 -0400

    snec

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', 'G-LK521ZYKJE');
           `}
         </Script>
+
+        {/* ✅ AdSense verification/ad script */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7617760862667547"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body className="flex flex-col min-h-screen bg-gray-100">
         <Header />
